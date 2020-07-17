42ba93f45a5ec805cad4ffbad7c3a7d2c5171e22
Xamran <60073088+Xamran@users.noreply.github.com>
2020-07-17 08:41:33 +0500

    File Upload

---
+++ TicketBooking/TicketBooking/Controllers/AdminController.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketBooking.Models;
+
+namespace TicketBooking.Controllers
+{
+    public class AdminController : Controller
+    {
+        TicketBookingEntities _context = new TicketBookingEntities();
+        // GET: Admin
+        public ActionResult Index(int? Id)
+        {
+            if (Session["Id"]  != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(Admin admin)
+        {
+            var result = _context.Admins.FirstOrDefault(o => o.UserName.Equals(admin.UserName) && o.Password.Equals(admin.Password));
+            if (result != null)
+            {
+                Session["Id"] = result.Id;
+                Session["UserName"] = result.UserName;
+                return RedirectToAction("Index");
+            }
+            TempData["Message"] = "Invalid User Details";
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("LogIn");
+        }
+        public ActionResult BusService(BusService busService, HttpPostedFileBase Image)
+        {
+            if (Session["Id"] != null)
+            {
+                return View(_context.BusServices.ToList());
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        public ActionResult AddBusService(int? Id, HttpPostedFileBase Image)
+        {
+            if (Session["Id"] != null)
+            {
+                var busService = _context.BusServices.Where(o => o.Id == Id).FirstOrDefault();
+                if (busService != null)
+                {
+                    var find = _context.BusServices.Find(Id);
+                    return View(find);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        [HttpPost]
+        public ActionResult AddBusService(BusService busService,HttpPostedFileBase UpdateImage, HttpPostedFileBase Image)
+        {
+            if (busService.Id > 0)
+            {
+                if (UpdateImage != null)
+                {
+                    string filename = UpdateImage.FileName;
+                    busService.Image = filename;
+                    string path = Path.Combine(Server.MapPath("/Images"), filename);
+                    UpdateImage.SaveAs(path);
+                }
+                _context.Entry(busService).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("BusService");
+            }
+            else
+            {
+                string filename = Image.FileName;
+                busService.Image = filename;
+                string path = Path.Combine(Server.MapPath("/Images"), filename);
+                Image.SaveAs(path);
+                _context.BusServices.Add(busService);
+                _context.SaveChanges();
+                return RedirectToAction("BusService");
+            }
+        }
+        public ActionResult DeleteBusService(int Id)
+        {
+            var delete = _context.BusServices.Where(o=>o.Id==Id).FirstOrDefault();
+            _context.BusServices.Remove(delete);
+            _context.SaveChanges();
+            return RedirectToAction("BusService");
+        }
+        public ActionResult BusDetails(Bus bus,HttpPostedFileBase Image)
+        {
+            if (Session["Id"] != null)
+            {
+                return View(_context.Buses.ToList());
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        public ActionResult AddBusDetails(int? Id,HttpPostedFileBase Image)
+        {
+            if (Session["Id"] != null)
+            {
+                TempData["BusService"] = _context.BusServices.ToList();
+                var bus = _context.Buses.Where(o => o.Id == Id).FirstOrDefault();
+                if (bus != null)
+                {
+                    var find = _context.Buses.Find(Id);
+                    return View(find);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        [HttpPost]
+        public ActionResult AddBusDetails(Bus bus, HttpPostedFileBase UpdateImage,HttpPostedFileBase Image)
+        {
+            TempData["BusService"] = _context.BusServices.ToList();
+            if (bus.Id > 0)
+            {
+                if(UpdateImage != null)
+                {
+                    string filename = UpdateImage.FileName;
+                    bus.Image = filename;
+                    string path = Path.Combine(Server.MapPath("/Images"), filename);
+                    UpdateImage.SaveAs(path);
+                }
+                _context.Entry(bus).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("BusDetails");
+            }
+            else
+            {
+                string filename = Image.FileName;
+                bus.Image = filename;
+                string path = Path.Combine(Server.MapPath("/Images"), filename);
+                Image.SaveAs(path);
+                _context.Buses.Add(bus);
+                _context.SaveChanges();
+                return RedirectToAction("BusDetails");
+            }
+        }
+        public ActionResult DeleteBusDetails(int Id)
+        {
+            var deletebus = _context.Buses.Where(o => o.Id == Id).FirstOrDefault();
+            _context.Buses.Remove(deletebus);
+            _context.SaveChanges();
+            return RedirectToAction("BusDetails");
+        }
+        public ActionResult BusRoutes(int? Id)
+        {
+            if (Session["Id"] != null)
+            {
+                return View(_context.BusRoutes.ToList());
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        public ActionResult AddBusRoutes(int? Id)
+        {
+            if (Session["Id"] != null)
+            {
+                TempData["Route"] = _context.Routes.ToList();
+                TempData["Bus"] = _context.Buses.ToList();
+                var busRoutes = _context.BusRoutes.Where(o => o.Id == Id).FirstOrDefault();
+                if (busRoutes != null)
+                {
+                    var find = _context.BusRoutes.Find(Id);
+                    return View(find);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        [HttpPost]
+        public ActionResult AddBusRoutes(int? Id,BusRoute busRoute)
+        {
+            TempData["Route"] = _context.Routes.ToList();
+            TempData["Bus"] = _context.Buses.ToList();
+            if (busRoute.Id > 0)
+            {
+                _context.Entry(busRoute).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("BusRoutes");
+            }
+            else
+            {
+                _context.BusRoutes.Add(busRoute);
+                _context.SaveChanges();
+                return RedirectToAction("BusRoutes");
+            }
+        }
+        public ActionResult DeleteBusRoutes(int Id)
+        {
+            var deleteroute = _context.BusRoutes.Where(o => o.Id == Id).FirstOrDefault();
+            _context.BusRoutes.Remove(deleteroute);
+            _context.SaveChanges();
+            return RedirectToAction("BusRoutes");
+        }
+        public ActionResult Routes()
+        {
+            if (Session["Id"] != null)
+            {
+                return View(_context.Routes.ToList());
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        public ActionResult AddRoutes(int? Id)
+        {
+            if (Session["Id"] != null)
+            {
+                TempData["Cities"] = GetCities();
+                var routes = _context.Routes.Where(o => o.Id == Id).FirstOrDefault();
+                if (routes != null)
+                {
+                    var find = _context.Routes.Find(Id);
+                    return View(find);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return RedirectToAction("LogIn");
+            }
+        }
+        [HttpPost]
+        public ActionResult AddRoutes(Route route)
+        {
+            TempData["Cities"] = GetCities();
+            if (route.Id > 0)
+            {
+                _context.Entry(route).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Routes");
+            }
+            else
+            {
+                _context.Routes.Add(route);
+                _context.SaveChanges();
+                return RedirectToAction("Routes");
+            }
+        }
+        public ActionResult DeleteRoutes(int Id)
+        {
+            var deleteroute = _context.Routes.Where(o => o.Id == Id).FirstOrDefault();
+            _context.Routes.Remove(deleteroute);
+            _context.SaveChanges();
+            return RedirectToAction("Routes");
+        }
+        public List<Cities> GetCities()
+        {
+            var list = new List<Cities>();
+            var city1 = new Cities { Text = "Lahore", Value = "Lahore" };
+            var city2 = new Cities { Text = "Islamabad", Value = "Islamabad" };
+            var city3 = new Cities { Text = "Peshawar", Value = "Peshawar" };
+            var city4 = new Cities { Text = "Karachi", Value = "Karachi" };
+            var city5 = new Cities { Text = "Multan", Value = "Multan" };
+            var city6 = new Cities { Text = "Sahiwal", Value = "Sahiwal" };
+            var city7 = new Cities { Text = "Bahawalpur", Value = "Bahawalpur" };
+            var city8 = new Cities { Text = "Gujrat", Value = "Gujrat" };
+            var city9 = new Cities { Text = "Gujranwala", Value = "Gujranwala" };
+            var city10 = new Cities { Text = "Sialkot", Value = "Sialkot" };
+            list.Add(city1);
+            list.Add(city2);
+            list.Add(city3);
+            list.Add(city4);
+            list.Add(city5);
+            list.Add(city6);
+            list.Add(city7);
+            list.Add(city8);
+            list.Add(city9);
+            list.Add(city10);
+            return list;
+        }
+    }
+}
---
+++ TicketBooking/TicketBooking/Controllers/HomeController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketBooking.Models;
+
+namespace TicketBooking.Controllers
+{
+    public class HomeController : Controller
+    {
+        TicketBookingEntities _context = new TicketBookingEntities();
+        public ActionResult Index()
+        {
+            return View(_context.BusServices.ToList());
+        }
+        public ActionResult BusServices(int? Id)
+        {
+            var busService = _context.BusServices.FirstOrDefault();
+            var bus = _context.Buses.Where(o => o.BusServiceId == Id).ToList();
+            return View(bus);
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(User user)
+        {
+            var result = _context.Users.FirstOrDefault(o => o.Email.Equals(user.Email) && o.Password.Equals(user.Password));
+            if (result != null)
+            {
+                Session["Id"] = result.Id;
+                return RedirectToAction("BookingDetails");
+            }
+            TempData["Message"] = "Invalid User Details";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BookingServices()
+        {
+            return View(_context.BusRoutes.ToList());
+        }
+        public ActionResult BookingDetails()
+        {
+            TempData["Cities"] = GetCities();
+            TempData["BusRoute"] = _context.BusRoutes.ToList();
+            return View(_context.BusRoutes.ToList());
+        }
+        [HttpPost]
+        public ActionResult BookingDetails(Route route,BusRoute busRoute)
+        {
+            TempData["Cities"] = GetCities();
+            TempData["BusRoute"] = _context.BusRoutes.ToList();
+            var search = _context.BusRoutes.Where(o => o.Route.FromRoute.Contains(route.FromRoute) && o.Route.ToRoute.Contains(route.ToRoute) && o.DepartureTime == busRoute.DepartureTime).ToList();
+            if (search.Count() != 0)
+            { 
+            return View(search);
+            }
+            else
+            {
+                return RedirectToAction("BookingDetails");
+            }
+        }
+        public ActionResult Book(int? Id)
+        {
+            var check = _context.BusRoutes.Where(o => o.Id == Id).FirstOrDefault();
+            return View(check);
+        }
+        [HttpPost]
+        public ActionResult Book(int? Id,int BusRouteId, BusRoute busRoute,UserTicket userTicket)
+        {
+            var route = _context.BusRoutes.Where(o => o.Id == userTicket.BusRouteId).FirstOrDefault();
+            var fare = _context.Buses.Where(o => o.Id == route.BusId).FirstOrDefault();
+            List<string> tickets = new List<string>();
+            for(var i = 1; i <= userTicket.ReservedSeats; i++)
+            {
+                var randomTicket = Guid.NewGuid().ToString().Substring(0, 6);
+                tickets.Add(randomTicket);
+            }
+            string multipleTickets = string.Join(",", tickets);
+            userTicket.TicketId = multipleTickets;
+            _context.SaveChanges();
+            if (userTicket.ReservedSeats >= 1)
+            { 
+                userTicket.PaidFare = fare.Price * userTicket.ReservedSeats;
+            }
+            busRoute.Id = route.Id;
+            busRoute.BusId = route.BusId;
+            busRoute.RouteId = route.RouteId;
+            busRoute.ArrivalTime = route.ArrivalTime;
+            busRoute.DepartureTime = route.DepartureTime;
+            busRoute.ReservedSeats = route.ReservedSeats + userTicket.ReservedSeats;
+            _context.Set<BusRoute>().AddOrUpdate(busRoute);
+            _context.SaveChanges();
+            _context.UserTickets.Add(userTicket);
+            _context.SaveChanges();
+            return RedirectToAction("Ticket", new { userTicket.Id });
+        }
+        public ActionResult Ticket(int Id)
+
+        {
+            var ticket = _context.UserTickets.Find(Id);
+            return View(ticket);
+        }
+        public List<Cities> GetCities()
+        {
+            var list = new List<Cities>();
+            var city1 = new Cities { Text = "Lahore", Value = "Lahore" };
+            var city2 = new Cities { Text = "Islamabad", Value = "Islamabad" };
+            var city3 = new Cities { Text = "Peshawar", Value = "Peshawar" };
+            var city4 = new Cities { Text = "Karachi", Value = "Karachi" };
+            var city5 = new Cities { Text = "Multan", Value = "Multan" };
+            var city6 = new Cities { Text = "Sahiwal", Value = "Sahiwal" };
+            var city7 = new Cities { Text = "Bahawalpur", Value = "Bahawalpur" };
+            var city8 = new Cities { Text = "Gujrat", Value = "Gujrat" };
+            var city9 = new Cities { Text = "Gujranwala", Value = "Gujranwala" };
+            var city10 = new Cities { Text = "Sialkot", Value = "Sialkot" };
+            list.Add(city1);
+            list.Add(city2);
+            list.Add(city3);
+            list.Add(city4);
+            list.Add(city5);
+            list.Add(city6);
+            list.Add(city7);
+            list.Add(city8);
+            list.Add(city9);
+            list.Add(city10);
+            return list;
+        }
+    }
+}
